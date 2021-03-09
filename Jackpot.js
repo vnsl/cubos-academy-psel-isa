08ee0d339f617a1d2e1a3d3b7a4471e4ca3cb830
Vinicius Nattan <Vinicius@Viniciuss-MacBook-Pro.local>
2021-03-09 20:25:20 -0300

    Jackpot

---
+++ Jackpot.js
@@ -0,0 +1,10 @@
+function solucao(resultado1, resultado2, resultado3) {
+	// seu código aqui	
+    // const acerto = resultado1 === resultado2 && resultado1 === resultado3
+    if (resultado1 === resultado2 && resultado1 === resultado3){
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+    
+    }
