b8020d75279a163377bfd5a29740615587ec4c4c
Vinicius Nattan <Vinicius@Viniciuss-MacBook-Pro.local>
2021-03-09 20:25:42 -0300

    Divisivel.

---
+++ Divisivel_por_X.js
@@ -0,0 +1,10 @@
+function solucao(numero, x) {
+	// seu código aqui
+    let resto = numero % x;
+    if (resto === 0) {
+        console.log(resto === 0);
+        } else if (resto !== 0) {
+            console.log(false);
+        }
+     
+}
